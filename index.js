5f78bf79cca2b894759ffec6ae51205ee7f65e0e
chelsea-banke <bankechelsea@gmail.com>
2022-03-02 21:16:25 +0100

    making items display more dynamic with JS

--- index.js
+++ index.js
@@ -6,12 +6,23 @@ class item {
         this.disc = discount;
     }
 }
-
 let promo = [
     new item("Power drill", "power-drill.jpg", "300.1$", "17%"),
     new item("Paint brush", "paint-brush.jpg", "50.6$", "5%"),
     new item("Wheelbarrow", "wheelbarrow.jpg", "120.99$", "23%"),
     new item("Saw", "saw.jpg", "75.0$", "9%")
+]
+let steels = [
+    new item("steel rods", "rods.jpg", "10.0$", "0%"),
+    new item("iron rods", "rust rods.jpg", "9.2$", "3%"),
+    new item("steel bars", "square bars.jpg", "15.5$", "0%"),
+    new item("steal weave", "metal weave.jpg", "15.0$", "0%")
+]
+let woods = [
+    new item("texture-1", "wood1.jpg", "20$", "9%"), 
+    new item("texture-2", "wood2.jpg", "23.0$", "3%"),
+    new item("texture-3", "wood3.jpg", "22.$", "0%"),
+    new item("texture-4", "wood4.jpg", "25.0$", "0%")
 ]
 promo.forEach(_item => {
     let itemContainer = document.createElement("div");
@@ -29,4 +40,42 @@ promo.forEach(_item => {
     </a>
 </div>`
 document.getElementById("promo-section").append(itemContainer);
+})
+steels.forEach(steel => {
+    let itemContainer = document.createElement("div");
+    itemContainer.classList.add("col-md-3");
+    itemContainer.innerHTML = `
+    <div class="thumbnail">
+        <a href="">
+            <img src="/${steel.source}" alt="${steel.name}" style="width: 100%;">
+            <div class="card">
+                <div class="row card-footer">
+                    <h5 class="col-5">${steel.name}</h5>
+                    <p class="col-7">${steel.price}/m<span class="text-success"> ${steel.disc} off</span></p>
+                    <button type="button" class="btn btn-success">shop item<button>
+                </div>
+            </div> 
+        </a>
+    </div>
+    `;
+    document.getElementById("steel-section").append(itemContainer);
+})
+woods.forEach(wood => {
+    let itemContainer = document.createElement("div");
+    itemContainer.classList.add("col-md-3");
+    itemContainer.innerHTML = `
+    <div class="thumbnail">
+        <a href="">
+            <img src="/${wood.source}" alt="${wood.name}" style="width: 100%;">
+            <div class="card">
+                <div class="row card-footer">
+                    <h5 class="col-5">${wood.name}</h5>
+                    <p class="col-7">${wood.price}/m<sup>2</sup> <span class="text-success"> ${wood.disc} off</span></p>
+                    <button type="button" class="btn btn-success">shop item<button>
+                </div>
+            </div> 
+        </a>
+    </div>
+    `;
+    document.getElementById("wood-section").append(itemContainer);
 })
