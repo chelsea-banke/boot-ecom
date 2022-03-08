7c1eea0dd922efd056f555a201f5fdb35a9366ba
chelsea-banke <bankechelsea@gmail.com>
2022-03-09 00:35:48 +0100

    enabling carousel display swiching

--- index.js
+++ index.js
@@ -12,15 +12,15 @@ class item {
 let allItems = [
     new item("Power drill", "tool", "power-drill.jpg", "300.1", "17", true),
     new item("Paint brush", "tool", "paint-brush.jpg", "50.6", "5"),
-    new item("Wheelbarrow", "tool", "wheelbarrow.jpg", "120.99", "23"),
+    new item("Wheelbarrow", "tool", "wheelbarrow.jpg", "120.99", "23", true),
     new item("Saw", "saw.jpg", "tool", "75.0$", "9%"),
     new item("steel rods", "steel", "rods.jpg", "10.0", "0"),
     new item("iron rods", "steel", "rust rods.jpg", "9.2", "3"),
-    new item("steel bars", "steel", "square bars.jpg", "15.5", "0"),
+    new item("steel bars", "steel", "square bars.jpg", "15.5", "0", true),
     new item("steal weave", "steel", "metal weave.jpg", "15.0", "0"),
     new item("texture-1", "wood", "wood1.jpg", "20", "9"), 
     new item("texture-2", "wood", "wood2.jpg", "23.0", "3"),
-    new item("texture-3", "wood", "wood3.jpg", "22.0", "0"),
+    new item("texture-3", "wood", "wood3.jpg", "22.0", "0", true),
     new item("texture-4", "wood", "wood4.jpg", "25.0", "0")
 ];
 
@@ -65,8 +65,26 @@ allItems.forEach(product => {
     index ++;
 })
 
-let modalItems = document.querySelectorAll(".modal-item");
+let carouselItems = document.querySelectorAll(".carousel-item");
+let switchIndex = 0;
+carouselItems[switchIndex].style.display = "block";
+switchCarousel = function() {
+    console.log(carouselItems.length);
+    switchIndex = (switchIndex+1) % (carouselItems.length);
+    console.log("loaded ", switchIndex);
+    if (switchIndex === 0){
+        let temp = carouselItems.length - 1;
+        carouselItems[temp].style.display = "none";
+    }
+    else{
+        carouselItems[switchIndex-1].style.display = "none";
+    }
+    carouselItems[switchIndex].style.display = "block";
+    console.log("loaded ", switchIndex);
+};
+document.body.onload(setInterval(switchCarousel, 4000));
 
+let modalItems = document.querySelectorAll(".modal-item");
 let itemImages = document.querySelectorAll(".item-image");
 itemImages.forEach(itemImage => {
     itemImage.addEventListener("click", function(){
